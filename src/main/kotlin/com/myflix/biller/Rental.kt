98061f17ba284bd21ffd93cfb93b90e039a9884a
Prajakta <prajaktar35@gmail.com>
2021-06-25 18:08:43 +0530

    Day1 Story 1

--- src/main/kotlin/com/myflix/biller/Rental.kt
+++ src/main/kotlin/com/myflix/biller/Rental.kt
@@ -1,3 +1,28 @@
 package com.myflix.biller
 
-class Rental(val movie: Movie, val daysRented: Int)
+class Rental(val movie: Movie, val daysRented: Int) {
+
+    fun amount(): Double {
+        var amount = 0.0
+        when (movie.priceCode) {
+            Movie.REGULAR -> {
+                amount += 2.0
+                if (daysRented > 2) amount += (daysRented - 2) * 1.5
+            }
+            Movie.NEW_RELEASE -> amount += daysRented * 3
+            Movie.CHILDRENS -> {
+                amount += 1.5
+                if (daysRented > 3) amount += (daysRented - 3) * 1.5
+            }
+        }
+        return amount
+    }
+
+    fun frequentRenterPoints(): Int {
+        // add bonus for a two day new release rental
+        return if (movie.priceCode == Movie.NEW_RELEASE && daysRented > 1)
+            2
+        else
+            1
+    }
+}
--- src/test/kotlin/com/myflix/biller/CustomerTest.kt
+++ src/test/kotlin/com/myflix/biller/CustomerTest.kt
@@ -10,4 +10,39 @@ class CustomerTest {
         1 + 1 shouldBe 2
     }
 
+    @Test
+    fun `should return statement`() {
+        val customer = Customer("John")
+        customer.addRental(Rental(Movie("Tenet", Movie.NEW_RELEASE), 4))
+        customer.addRental(Rental(Movie("Harry Potter", Movie.CHILDRENS), 2))
+        customer.addRental(Rental(Movie("Interstellar", Movie.REGULAR), 3))
+
+        val actualStatement = customer.statement()
+
+        actualStatement shouldBe "Rental Record for John\n" +
+                "\tTenet\t12.0\n" +
+                "\tHarry Potter\t1.5\n" +
+                "\tInterstellar\t3.5\n" +
+                "Amount owed is 17.0\n" +
+                "You earned 4 frequent renter points\n"
+    }
+
+    @Test
+    fun `should return html statement`() {
+        val customer = Customer("John")
+        customer.addRental(Rental(Movie("Tenet", Movie.NEW_RELEASE), 4))
+        customer.addRental(Rental(Movie("Harry Potter", Movie.CHILDRENS), 2))
+        customer.addRental(Rental(Movie("Interstellar", Movie.REGULAR), 3))
+
+        val actualHtmlStatement = customer.htmlStatement()
+
+        actualHtmlStatement shouldBe "<h3>Rental Record for John</h3></br>" +
+                "<p>" +
+                "Tenet: <b>12.0</b></br>" +
+                "Harry Potter: <b>1.5</b></br>" +
+                "Interstellar: <b>3.5</b></br>" +
+                "</p>" +
+                "<p>Amount owed is <b>17.0</b></p>" +
+                "You earned 4 frequent renter points"
+    }
 }
